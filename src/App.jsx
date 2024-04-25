f935d1a029e7168e17a7aaef20deee3b19d252b8
Aviral000 <avimalik02@gmail.com>
2024-04-25 21:55:14 +0530

    c2

--- src/App.jsx
+++ src/App.jsx
@@ -4,18 +4,33 @@ import './App.css';
 export default function App() {
   const [expression, setExpression] = useState('');
   const [result, setResult] = useState(0);
+  const [displayCalc, setDisplayCalc] = useState(false);
 
   const handleButtonClick = (value) => {
     if (value === '=') {
+        setDisplayCalc(true);
       try {
-        const evaluatedResult = eval(expression);
-        setResult(evaluatedResult);
+        if (expression === '') {
+          setResult('Error');
+        } else {
+          const evaluatedResult = eval(expression);
+          if (evaluatedResult === Infinity) {
+            setResult('Infinity');
+          } else if (isNaN(evaluatedResult)) {
+            setResult('NaN');
+          } else if (evaluatedResult === 0) {
+            setResult(0);
+          } else {
+            setResult(evaluatedResult);
+          }
+        }
       } catch (error) {
         setResult('Error');
       }
     } else if (value === 'C') {
       setExpression('');
       setResult(0);
+      setDisplayCalc(false);
     } else {
       setExpression(expression + value);
     }
@@ -25,7 +40,7 @@ export default function App() {
     <div className='container'>
       <h1>React Calculator</h1>
       <input type="text" value={expression} onChange={(e) => setExpression(e.target.value)} />
-      {result !== 0 && <h5 className='result'>{result}</h5>}
+      {displayCalc && <h5 className='result'>{result}</h5>}
       <div>
         <div className='buttons'>
           <button type='button' onClick={() => handleButtonClick('7')}>7</button>
